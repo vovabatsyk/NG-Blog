8e1c5f6f5df7601a46b7aee0b8e0ca626ce589ea
Vova Batsyk <vovabatsyk@gmail.com>
2017-09-27 11:30:40 +0000

    “Initials”

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-
 let userNum = Number(prompt('Enter the number of the Fibonacci'));
-function fib(n){
-	if (n==1||n==2) return 1;
-	else return fib(n-2)+fib(n-1);		
-	}
+function fib(n) {
+    if (n == 1 || n == 2)
+        return 1;
+    else
+        return fib(n - 2) + fib(n - 1);
+}
 document.write(fib(userNum));
