be428830e1761d47c002f65885ac39862853475d
vovabatsyk <vovabatsyk.lviv@gmail.com>
2021-10-25 21:20:59 +0300

    Add comments

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,49 +1,43 @@
-import { Router } from '@angular/router'
-import { User } from './../../shared/interfaces'
-import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup, Validators } from '@angular/forms'
-import { AuthService } from '../shared/services/auth.service'
+import { Router } from '@angular/router';
+import { User } from './../../shared/interfaces';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
+  form: FormGroup;
+  submitted = false;
 
-  form: FormGroup
-  submitted = false
-
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl(null,
-        [
-          Validators.required,
-          Validators.email
-        ]),
-      password: new FormControl(null,
-        [
-          Validators.required,
-          Validators.minLength(6)
-        ]),
-    })
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   }
-
+  // submit
   submit() {
-    if (this.form.invalid) return
-    this.submitted = true
+    if (this.form.invalid) return;
+    this.submitted = true;
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password,
-    }
+    };
 
-    if (user) this.auth.login(user).subscribe(() => {
-      this.form.reset()
-      this.router.navigate(['/admin', 'dashboard'])
-      this.submitted = false
-    })
+    if (user)
+      this.auth.login(user).subscribe(() => {
+        this.form.reset();
+        this.router.navigate(['/admin', 'dashboard']);
+        this.submitted = false;
+      });
   }
-
 }
